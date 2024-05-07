9a671fb06162a630c6c99c6b74541905b8e641ea
Ronney Arias <ronneyarias@gmail.com>
2024-05-07 17:24:55 -0500

    complete lab

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,18 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    if length == 0:
+        print([])
+    elif length == 1:
+        print([0])
+    elif length == 2:
+        print([0, 1])
+    else:
+        sequence = [0, 1]
+        while len(sequence) < length:
+            next_num = sequence[-1] + sequence[-2]
+            sequence.append(next_num)
+        print(sequence)
+    
+print(print_fibonacci(10))
+# output [0, 1, 1, 2, 3, 5, 8, 13, 21, 34]
